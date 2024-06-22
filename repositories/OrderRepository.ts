e694fa0ba853ec50672efa86056109a3274416c2
s99100532 <s99100532@gmail.com>
2024-06-22 21:08:00 +0800

    fix: fix error handlnng

--- repositories/OrderRepository.ts
+++ repositories/OrderRepository.ts
@@ -38,6 +38,7 @@ export default class OrderRepository extends Repository {
         const createOrderStmt =
           `INSERT INTO orders (user_id, status) values (?, ?)`;
         const orderStatus = OrderStatus.CREATED;
+
         const orderCreateResult = await conn.execute(createOrderStmt, [
           userId,
           orderStatus,
@@ -53,26 +54,41 @@ export default class OrderRepository extends Repository {
           `INSERT INTO order_item (order_id, product_id) values
           ${orderProducts.map(() => "(?, ?)").join(",")}`;
 
-        await conn.execute(
+        const createItemResult = await conn.execute(
           createItemsStmt,
           orderProducts
             .map((pd) => [orderId, pd.id])
             .flat(),
         );
 
+        if (!createItemResult.affectedRows) {
+          throw new Error("items create fail");
+        }
+
         const updateQuantityStmt =
           `UPDATE products SET quantity = quantity - 1 where id in 
           (${orderProducts.map((_) => "?").join(",")})`;
 
-        await conn.execute(
+        const updateQuantityResult = await conn.execute(
           updateQuantityStmt,
           orderProducts.map((p) => p.id),
         );
 
+        if (updateQuantityResult.affectedRows != orderProducts.length) {
+          throw new Error("quantity update fail");
+        }
+
         const updateBalanceStmt =
           `UPDATE users SET balance = balance - ? where id = (?) `;
 
-        await conn.execute(updateBalanceStmt, [amount, userId]);
+        const updateBalanceResult = await conn.execute(updateBalanceStmt, [
+          amount,
+          userId,
+        ]);
+
+        if (updateBalanceResult.affectedRows != 1) {
+          throw new Error("balance update fail");
+        }
 
         return {
           id: orderId,
